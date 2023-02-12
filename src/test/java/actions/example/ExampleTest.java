140b7b096796b53c36a4be721a37df4a4b0194fb
Jesús Sánchez Cuadrado <jesusc@um.es>
2023-02-12 23:23:11 +0100

    Initial commit

---
+++ src/test/java/actions/example/ExampleTest.java
@@ -0,0 +1,14 @@
+package actions.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void testName() throws Exception {
+		assertTrue(true);
+	}
+	
+}
