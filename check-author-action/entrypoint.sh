f19893b74523e94dfbb7b4f1388054e39244a0da
Jesús Sánchez Cuadrado <jesusc@um.es>
2023-02-14 14:15:50 +0100

    Debugging info

--- check-author-action/entrypoint.sh
+++ check-author-action/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/sh -l
 
-NUM=`find -type f | grep java$ | wc -l`
-find -type f | grep java$ | grep "@author ${INPUT_WHO}" >> $GITHUB_OUTPUT
+echo "::debug::{Running...}"
 
-TOTAL=`find -type f | grep java$ | grep "@author ${INPUT_WHO}" | wc -l`
+NUM=`find -type f | grep java$ | wc -l`
+# find -type f | grep java$ | grep "@author ${INPUT_WHO}" >> $GITHUB_OUTPUT
+
+TOTAL=`find -type f | grep java$ | xargs grep "@author ${INPUT_WHO}" | wc -l`
 
 number_of_files=$NUM
 
 if [ $NUM -ne $TOTAL ]; then
+  echo "::debug::{There are $NUM files with $TOTAL authors}"
   exit 1
 fi
+
+
+echo "::debug::{Everything is ok $NUM = $TOTAL }"
+exit 0
